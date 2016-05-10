bd99d60930e8b883e306bcd1c42113564df7544c
Thomas Khaipi <khaipi@gmail.com>
2016-05-10 19:50:38 +0200

    add security role

--- src/main/java/com/example/controller/PostController.java
+++ src/main/java/com/example/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/posts")
 public class PostController {
 	
+	@Secured("ROLE_GUEST")
 	@RequestMapping("/list")
 	public String list(){
 		return "list posts....";
 	}
 	
-	@RequestMapping("/admin/posts/create")
+	@Secured("ROLE_USER")
+	@RequestMapping("/drafts")
+	public String viewDrafts(){
+		return "add drafts....";
+	}
+
+	//@Secured("ROLE_ADMIN")
+	@Secured({"ROLE_ADMIN","ROLE_USER"})
+	@RequestMapping("/add")
 	public String addPost(){
 		return "add posts....";
 	}
